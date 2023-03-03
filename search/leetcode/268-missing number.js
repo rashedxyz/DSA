ae2ecaf1d4b91c68bfd82ad8a8520990a48498b1
rashedxyz <rashed.webdev@gmail.com>
2023-03-03 10:30:21 +0600

    leetcode 268- missing number updated

--- search/leetcode/268-missing number.js
+++ search/leetcode/268-missing number.js
@@ -1,4 +1,8 @@
-const missingNumber = (nums) => {
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
   let sum = 0;
   for (let i = 0; i <= nums.length; i++) {
     sum = sum + i;
@@ -10,8 +14,4 @@ const missingNumber = (nums) => {
   }
 
   return sum - sum2;
-}
-
-const numbers = [4, 5, 1, 2, 0, 6]; // range [0, 6] distinct length 6
-
-console.log(missingNumber(numbers));
+};
